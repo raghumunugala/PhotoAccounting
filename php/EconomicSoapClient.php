c67dc143555482ae1ebae883f7d7af635c4f8f4f
igreactive <igreactive@gmail.com>
2012-08-26 03:31:35 +0200

    Include credentials from config while connecting Added getAccounts function

--- php/EconomicSoapClient.php
+++ php/EconomicSoapClient.php
@@ -7,7 +7,6 @@
 class EconomicSoapClient {
 	private static $client;
 	
-	
 	function __construct() {
 		$this->connect();
 	}
@@ -17,17 +16,37 @@ class EconomicSoapClient {
 	 * http://apiforum.e-conomic.com/soap-f8/economic-api-exceptions-authorizationexception-t4678.html
 	 */
 	private function connect() {
+		include_once 'includes/web_service_credentials.php';
 		ini_set('max_execution_time', 0);
 		try {
 			$this->client = new SoapClient("https://www.e-conomic.com/secure/api1/EconomicWebservice.asmx?WSDL", array("trace" => 1, "exceptions" => 1));
-		
-			$this->client->Connect(array(
-					'agreementNumber' => "438582",
-					'userName' => "Dav",
-					'password' => "a5wht4kn"
-			));
+			$this->client->Connect($credentials);
 		} catch (SoapFault $fault) {
 			trigger_error(sprintf("Soap fault %s - %s", $fault->faultcode, $fault->faultstring), E_USER_ERROR);
 		}
+	}
+	
+	public function getAccounts(){
+		$accounts = array();
+		
+		try {
+			$accounts_result = $this->client->Account_GetAll()->Account_GetAllResult;
+			
+			if (is_object($accounts_result) && property_exists($accounts_result, 'AccountHandle')) {
+				$accounts_handle = $this->client->Account_GetDataArray(array(
+						'entityHandles' => $accounts_result->AccountHandle
+				))->Account_GetDataArrayResult;
+			
+				if (is_object($accounts_handle) && property_exists($accounts_handle, 'AccountData')) {
+					$accounts = $accounts_handle->AccountData;
+				} else {
+					throw new Exception('No Account data available '. $e->getMessage());
+				}
+			}			
+		} catch (Exception $e) {
+			throw new Exception('Accounts could not be returned '. $e->getMessage());
+		}
+		
+		return $accounts;
 	}
 }

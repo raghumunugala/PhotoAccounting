0a13f05fbcf616d1caf2bfdec903ec5f59a23a7b
igreactive <igreactive@gmail.com>
2012-08-28 00:28:05 +0200

    Replace Balance field with VAT

--- js/accountsTable.js
+++ js/accountsTable.js
@@ -1,27 +1,32 @@
-YUI().use('datatable-scroll', "datasource-io", "datasource-jsonschema", "datatable-datasource", function (Y) {
-    var dataSource = new Y.DataSource.IO({ 
-    	source: "php/economic-ajax.php" 
-    });
-    
-    dataSource.plug(Y.Plugin.DataSourceJSONSchema, {
-    	schema: {
-    		resultFields: ["Number","Name","Balance"]
-        }
-    });
-    
-    var columns = [
-        { key: "Number", label: "No." },
-        { key: "Name", label: "Name" },
-        { key: "Balance", label: "Balance" }
-    ];
-    
-    var table = new Y.DataTable({
-        caption: "Accounts Data",
-        columns: columns,
-        scrollable: "y",
-        height:"300px"})
-    	.plug(Y.Plugin.DataTableDataSource, { datasource: dataSource });
-    
-    table.render('#economicAccountsData');
-    table.datasource.load();
-});
+YUI().use('datatable-scroll', "datasource-io", "datasource-jsonschema",
+		"datatable-datasource", function(Y) {
+			var columns = [ {
+				key : "Number",
+				label : "No."
+			}, 'Name', 'VAT' ];
+
+			var dataSource = new Y.DataSource.IO({
+				source : "php/economic-ajax.php"
+			});
+
+			dataSource.plug(Y.Plugin.DataSourceJSONSchema, {
+				schema : {
+					resultFields : [ "Number", "Name", {
+						key : 'VAT',
+						locator : "VatAccountHandle.VatCode"
+					} ]
+				}
+			});
+
+			var table = new Y.DataTable({
+				caption : "Accounts Data",
+				columnset : columns,
+				scrollable : "y",
+				height : "300px"
+			}).plug(Y.Plugin.DataTableDataSource, {
+				datasource : dataSource
+			});
+
+			table.render('#economicAccountsData');
+			table.datasource.load();
+		});
